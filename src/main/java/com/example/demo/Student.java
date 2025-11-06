8a6a37b4211edee8abfb0a912479205ee4e81d96
yuanjing <jing.yuan@kapeixi.com>
2025-11-06 16:48:08 +0800

    .

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -17,8 +17,57 @@ public class Student {
     private String name;
 
     private int age;
+    private int gender;
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student(int gender) {
+        this.gender = gender;
+    }
+
+    public Student(int gender, int age) {
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Student(int gender, int age, String name) {
+        this.gender = gender;
+        this.age = age;
+        this.name = name;
+    }
 
     public Student() {
+
     }
 
     public Student(String name, int age) {
